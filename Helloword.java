6a82b61c845effcaade6b6ca28b23bd60b14039a
m-viniciuss <mvinicius@alu.ufc.br>
2021-08-26 11:45:29 -0300

    Create Helloword.java

---
+++ Helloword.java
@@ -0,0 +1,11 @@
+public class Helloword{
+  //Declaração de Variáveis
+    private String saudacao;
+    
+    //Construtor de classe
+    public Helloword (String n){
+        saudacao = n;
+        System.out.print(saudacao);
+    }
+    //Teste aula01
+}
